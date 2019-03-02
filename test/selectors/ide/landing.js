b8f88537e9c0b0464920626c7c9f06d560a0d012
jonathan-gartland <jonathan.gartland@rstudio.com>
2019-03-01 19:49:17 -0500

    stubbed selector json

--- test/selectors/ide/landing.js
+++
@@ -1,9 +0,0 @@
-export default   {
-  landingSelectors : {
-    'menuLinks': '#currentUser > div > div.menuItems > div > a',
-    'menuToggler': '#menuToggler',
-    'accountNavLogin': '#userPanel > div > div.menu > a.menuItem.login',
-    'accountNavSignup': '#userPanel > div > div.menu > a.menuItem.signup',
-    'footerLogo': '#footerLogo'
-  }
-};
