c085b1aa34129feffb89589bf42b47b40100589d
jonathan-gartland <jonathan.gartland@gmail.com>
2019-03-29 08:39:18 -0400

    really screwed up that merge with the selectors branch. I think I did it backwards by accident. but should be ok now.

--- test/pageobjects/cloudide/project.page.js
+++ test/pageobjects/cloudide/project.page.js
@@ -11,31 +11,71 @@ class ProjectPage extends Page {
   // right nav elements, space menu, ....
   // might get broken out into it's own object - try to think nimbly
   // this probably the space 'page' at the end of the day
-  get spaceNavToggler() { return $(selectors.cloudIde.projectPage.spaceNavToggler); }
+  get spaceNavToggler() { return $(selectors.cloudIdeCss.projectPage.spaceNavToggler); }
   // similar to above
-  get yourProjects() { return $(selectors.cloudIde.projectPage.yourProjects); }
-  get newProjectButton() { return $(selectors.cloudIde.projectPage.newProjectButton); }
+  get yourProjects() { return $(selectors.cloudIdeCss.projectPage.yourProjects); }
+  get newProjectButton() { return $(selectors.cloudIdeCss.projectPage.newProjectButton); }
+  get existingProjectLink() { return $(selectors.cloudIdeCss.projectPage.existingProject); }
 
-  get accountSelectPopUp() { return $(selectors.cloudIde.accountPopUp.modalDialog); }
-  get accountFieldToSelect() { return $(selectors.cloudIde.accountPopUp.accountFromList); }
-  get accountSelectButton() { return $(selectors.cloudIde.accountPopUp.submitButton); }
-  get systemStatus() { return $(selectors.cloudIde.projectPage.systemStatus); }
+  get accountSelectPopUp() { return $(selectors.cloudIdeCss.accountPopUp.modalDialog); }
+  get accountFieldToSelect() { return $(selectors.cloudIdeCss.accountPopUp.accountFromList); }
+  get accountSelectButton() { return $(selectors.cloudIdeCss.accountPopUp.submitButton); }
+  get systemStatus() { return $(selectors.cloudIdeCss.projectPage.systemStatus); }
 
   // get iFrame() { return $('#rstudio'); }
 
   // get iFrameRStudioElement() { return $('#rstudio_console_output'); }
 
-  //get ideConsole() { return $(selectors.cloudIde.projectPage.rstudioConsole); }
+  //get ideConsole() { return $(selectors.cloudIdeCss.projectPage.rstudioConsole); }
 
   // message while waiting: #contentContainer > div > div
 
   // content ~= project page object in this case, opening the project
   // will be an ide/iframe page object
 
+  deleteProject() {
+
+    // 2 ways via GUI
+
+  }
+
+  openExistingProject() {
+
+    //browser.debug();
+    browser.pause(5000);
+    this.existingProjectLink.waitForDisplayed(10000)
+
+    if (this.existingProjectLink.isExisting()) {
+      this.existingProjectLink.click();
+    }
+    browser.pause(15000);
+
+
+    //browser.debug();
+    // if (this.existingProjectLink.isExisting()) {
+    //   browser.debug();
+    //   this.existingProjectLink.click();
+    //
+    //   //$('#rstudio').waitForExist(90000);
+    //   //$('#contentFrame').waitForExist(90000);
+    //   $('#rstudio_console_output').waitForExist(90000);
+    //   if($('#rstudio_console_output').isExisting()) {
+    //     browser.debug();
+    //   }
+      //browser.switchToFrame('#rstudio_console_output')
+      //console.log($('#rstudio_console_output').isExisting());
+
+    //}
+    //browser.debug();
+    //browser.switchToFrame('#rstudio_console_output')
+
+  }
+
   openNewProject() {
     if (this.newProjectButton.isDisplayed()) {
       this.newProjectButton.click();
     }
+
 
     //var iFrame = "//*[@id='contentIFrame']"; //#contentIFrame
 
